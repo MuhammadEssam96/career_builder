098bb36f90964b8b647ca8740e7657750c4aeb53
MuhammadEssam96 <muhammad.essam96@gmail.com>
2020-06-05 20:23:26 +0200

    Refactor and reformat new_skills.dart

--- lib/screens/new_skills.dart
+++ lib/screens/new_skills.dart
@@ -23,24 +23,23 @@ class _NewSkillsState extends State<NewSkills> {
   fetchInpro() async {
     dbref.orderByChild('user_id').equalTo('2').once().then((snapshot) {
       if (snapshot.value != null) {
-        var respose = snapshot.value.values;
-        for (int i = 0; i < respose.length; i++) {
-          if (respose.elementAt(i)['status'] == 'new') {
-            var skillId = respose.elementAt(i)['skill_id'];
-            newSkills.add(respose.elementAt(i));
+        var response = snapshot.value.values;
+        for (int i = 0; i < response.length; i++) {
+          if (response.elementAt(i)['status'] == 'new') {
+            var skillId = response.elementAt(i)['skill_id'];
+            newSkills.add(response.elementAt(i));
             fetchSkills(skillId);
           }
         }
         if(newSkills.isEmpty){
-           skilldata.add({
-          'langname': 'no new skills yet',
-          'level': '0'
-        });
+          skilldata.add({
+            'langname': 'no new skills yet',
+            'level': '0'
+          });
             setState(() {});
         }
-
         print('new $newSkills');
-      }else{
+      } else {
         skilldata.add({
           'langname': 'no new skills yet',
           'level': '0'
@@ -75,9 +74,10 @@ class _NewSkillsState extends State<NewSkills> {
 
   @override
   Widget build(BuildContext context) {
-     int  skilllength=skilldata.length;
-             double equation=(skilllength*35).toDouble();
-              double sizedboxHeight=50 + equation ;
+    int skilllength = skilldata.length;
+    double equation = (skilllength*35).toDouble();
+    double sizedboxHeight = 50 + equation ;
+
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
@@ -92,7 +92,6 @@ class _NewSkillsState extends State<NewSkills> {
                 style: TextStyle(fontWeight: FontWeight.bold),
               ),
             ),
-
             SizedBox(
               width: 10,
             ),
@@ -112,75 +111,78 @@ class _NewSkillsState extends State<NewSkills> {
         ),
         //vertical
         Container(
-            margin: EdgeInsets.only(top: 3),
-            width: MediaQuery.of(context).size.width - 60,
-            height: 2,
-            color: Color(0xff8CDBD8)),
+          margin: EdgeInsets.only(top: 3),
+          width: MediaQuery.of(context).size.width - 60,
+          height: 2,
+          color: Color(0xff8CDBD8)
+        ),
         SizedBox(
-            height: sizedboxHeight,
-            child: skilldata.isNotEmpty
-                ? ListView.builder(
-                    itemCount: skilldata.length,
-                    physics: const NeverScrollableScrollPhysics(),
-                    itemBuilder: (context, index) {
-                      var level = skilldata[index]['level'];
-                      return Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+          height: sizedboxHeight,
+          child: skilldata.isNotEmpty ? ListView.builder(
+            itemCount: skilldata.length,
+            physics: const NeverScrollableScrollPhysics(),
+            itemBuilder: (context, index) {
+              var level = skilldata[index]['level'];
+              return Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Flexible(flex:3,
+                    child: Container(
+                      // color: Colors.indigoAccent,
+                      child: Column(
                         children: <Widget>[
-                          Flexible(flex:3,
-                            child: Container(
-                              // color: Colors.indigoAccent,
-                              child: Column(
-                                children: <Widget>[
-                                  Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: Text(
-                                      '${skilldata[index]['langname']}',
-                                      style:widget.style,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                          Flexible(flex: 1,
-                             child: Container(
-                              //  color:Colors.orange,
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: <Widget>[
-                                  newSkills.isNotEmpty? Row(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: <Widget>[
-                                      level == '1' || level == '2' || level == '3'
-                                          ? LevelPoints(containerColor: activeLevel)
-                                          : LevelPoints(
-                                              containerColor:
-                                                  inactiveLevel), //level 1 2 3
-                                      SizedBox(width: 1),
-                                      level == '2' || level == '3'
-                                          ? LevelPoints(containerColor: activeLevel)
-                                          : LevelPoints(
-                                              containerColor:
-                                                  inactiveLevel), //level 2 3
-                                      SizedBox(width: 1),
-                                      level == '3'
-                                          ? LevelPoints(containerColor: activeLevel)
-                                          : LevelPoints(
-                                              containerColor:
-                                                  inactiveLevel), //level 3
-                                    ],
-                                  ):Container(),
-                                ],
-                              ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              '${skilldata[index]['langname']}',
+                              style:widget.style,
                             ),
                           ),
                         ],
-                      );
-                    })
-                : Center(
-                    child: CircularProgressIndicator(),
-                  )),
+                      ),
+                    ),
+                  ),
+                  Flexible(flex: 1,
+                    child: Container(
+                      //  color:Colors.orange,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          newSkills.isNotEmpty? Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              level == '1' || level == '2' || level == '3' ?
+                              LevelPoints(
+                                containerColor: activeLevel
+                              )
+                              : LevelPoints(
+                                containerColor: inactiveLevel
+                              ), //level 1 2 3
+                              SizedBox(width: 1),
+                              level == '2' || level == '3' ?
+                              LevelPoints(containerColor: activeLevel)
+                              : LevelPoints(
+                                containerColor: inactiveLevel
+                              ), //level 2 3
+                              SizedBox(width: 1),
+                              level == '3' ? LevelPoints(containerColor: activeLevel)
+                              : LevelPoints(
+                                containerColor: inactiveLevel
+                              ), //level 3
+                            ],
+                          ):Container(),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              );
+            }
+          )
+          : Center(
+            child: CircularProgressIndicator(),
+          )
+        ),
       ],
     );
   }
