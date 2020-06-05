a2b08e973b376b1acd28f2744a56bda0ee895469
MuhammadEssam96 <muhammad.essam96@gmail.com>
2020-06-05 17:59:56 +0200

    Refactor and reformat congrate.dart and switch to StatefulWidget

--- lib/screens/congrate.dart
+++ lib/screens/congrate.dart
@@ -1,17 +1,27 @@
 import 'package:flutter/material.dart';
-class Congrate extends StatelessWidget {
+
+class Congrate extends StatefulWidget {
+  @override
+  _CongrateState createState() => _CongrateState();
+}
+
+class _CongrateState extends State<Congrate> {
   var question1Result;
   var question2Result;
-  // Congrate(this.question1Result,this.question2Result);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('your result'),),
+      appBar: AppBar(
+        title: Text('your result')
+      ),
       body: Container(
-      child: Column(children: <Widget>[
-        Text(question1Result)
-      ],)
-    ),
+        child: Column(
+          children: <Widget>[
+            Text(question1Result)
+          ],
+        )
+      ),
     );
   }
 }
