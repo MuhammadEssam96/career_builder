93be56f46038de7e9f4063b462e5cbe005d31474
MuhammadEssam96 <muhammad.essam96@gmail.com>
2020-06-05 16:14:17 +0200

    Refactor and reformat user.dart

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_database/firebase_database.dart';
 
-class User{
+class User {
   final String fName;
   final String lName;
   final String job;
@@ -11,26 +11,35 @@ class User{
   final String age;
   final String img;
   final userDbRef = FirebaseDatabase.instance.reference().child('users');
-  final List u=[];
+  final List u = [];
 
-  User({this.fName, this.lName, this.job, this.gender, this.education, this.email, this.phone, this.age, this.img});
-
-  
- fetchUser(){
-  userDbRef.child('id').equalTo('0').once().then(( snapshot){
-   var userResponse = snapshot.value;
-     print('userResponse1 $userResponse');
-      u.add({
-                 'fName': '${userResponse['first_name']}',
-                  'lName': '${userResponse['last_name']}',
-                  'job': '${userResponse['job']}',
-                  'gender': '${userResponse['gender']}',
-                  'age': '${userResponse['age']}',
-                  'education': '${userResponse['eduvation']}',
-                  'email': '${userResponse['email']}',
-                  'phone': '${userResponse['phone']}',
-                  'img': '${userResponse['profile_picture']}',
-    });
+  User({
+    this.fName,
+    this.lName,
+    this.job,
+    this.gender,
+    this.education,
+    this.email,
+    this.phone,
+    this.age,
+    this.img
   });
+
+  fetchUser() {
+    userDbRef.child('id').equalTo('0').once().then((snapshot) {
+      var userResponse = snapshot.value;
+      print('userResponse1 $userResponse');
+      u.add({
+        'fName': '${userResponse['first_name']}',
+        'lName': '${userResponse['last_name']}',
+        'job': '${userResponse['job']}',
+        'gender': '${userResponse['gender']}',
+        'age': '${userResponse['age']}',
+        'education': '${userResponse['eduvation']}',
+        'email': '${userResponse['email']}',
+        'phone': '${userResponse['phone']}',
+        'img': '${userResponse['profile_picture']}',
+      });
+    });
   }
-  }
+}
