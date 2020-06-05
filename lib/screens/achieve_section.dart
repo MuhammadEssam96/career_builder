0434867f3e60c6da119ad401bd4cf430996b857d
MuhammadEssam96 <muhammad.essam96@gmail.com>
2020-06-05 16:34:50 +0200

    Refactor and reformat achieve_section.dart

--- lib/screens/achieve_section.dart
+++ lib/screens/achieve_section.dart
@@ -2,47 +2,44 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-
 class AchieveSection extends StatefulWidget {
   @override
   _AchieveSectionState createState() => _AchieveSectionState();
 }
 
 class _AchieveSectionState extends State<AchieveSection> {
+  final dbRef = FirebaseDatabase.instance.reference().child('users_achievement');
+  final dbRef2 = FirebaseDatabase.instance.reference().child('achievement');
+  final List achievementsIds = [];
+  final List achievementsData = [];
 
-final dbRef=FirebaseDatabase.instance.reference().child('users_achievement');
-final dbRef2=FirebaseDatabase.instance.reference().child('achievement');
-final List achivementsIds=[];
-final List achivementsData=[];
- //fetch from user achievements
-  fetchUserAchieve(){
-    dbRef.orderByChild('user_id').equalTo('0').once().then((userSnapshot){
-    print('achivement snapshot ${userSnapshot.value}');
-    var userResponse=userSnapshot.value;
-    if(userSnapshot.value!=null){
-      for (var i = 0; i < userResponse.length; i++) {
-        achivementsIds.add(userResponse[i]['achievement_id']);
-        var achiveId=achivementsIds[i];
-        fetchAchievements(achiveId);
+  //fetch from user achievements
+  fetchUserAchieve() {
+    dbRef.orderByChild('user_id').equalTo('0').once().then((userSnapshot) {
+      print('achievement snapshot ${userSnapshot.value}');
+      var userResponse = userSnapshot.value;
+      if (userSnapshot.value != null) {
+        for (var i = 0; i < userResponse.length; i++) {
+          achievementsIds.add(userResponse[i]['achievement_id']);
+          var achievementId = achievementsIds[i];
+          fetchAchievements(achievementId);
+        }
+
+        if (achievementsIds.isEmpty) {
+          achievementsData.add('No Achievements yet.');
+          setState(() {});
+        }
+        print('achievements list $achievementsIds');
       }
-      if(achivementsIds.isEmpty){
-        achivementsData.add('No Achievements yet.');
-        setState(() {
-          
-        });
-      }
-      print('achivements list $achivementsIds');
-    }
     });
   }
-  //fetch from achivements
-  fetchAchievements(var achiveId){
-    dbRef2.child('$achiveId').once().then((achiveSnapshot){
-      print('achievement snapshot ${achiveSnapshot.value}');
-      achivementsData.add(achiveSnapshot.value['image']);
-      setState(() {
-        
-      });
+
+  //fetch from achievements
+  fetchAchievements(var achievementId) {
+    dbRef2.child('$achievementId').once().then((achievementSnapshot) {
+      print('achievement snapshot ${achievementSnapshot.value}');
+      achievementsData.add(achievementSnapshot.value['image']);
+      setState(() {});
     });
   }
 
@@ -51,58 +48,64 @@ final List achivementsData=[];
     super.initState();
     fetchUserAchieve();
   }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
-      Row(
-        children: <Widget>[
-          Container(
-            width: 50,
-            height: 50,
-            decoration: BoxDecoration(
-              color: Color(0xffA2E8E8),
-              borderRadius: BorderRadius.circular(30),
+        Row(
+          children: <Widget>[
+            Container(
+              width: 50,
+              height: 50,
+              decoration: BoxDecoration(
+                color: Color(0xffA2E8E8),
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: Icon(
+                FontAwesomeIcons.medal,
+                size: 35,
+              ),
             ),
-            child: Icon(
-              FontAwesomeIcons.medal,
-              size: 35,
+            SizedBox(
+              width: 10,
             ),
-          ),
-          SizedBox(
-            width: 10,
-          ),
-          //sachieve text
-          Container(
-            padding: EdgeInsets.only(top: 20),
-            child: Text(
-              'ACHIEVEMENT',
-              style: TextStyle(fontWeight: FontWeight.bold),
+            //achieve text
+            Container(
+              padding: EdgeInsets.only(top: 20),
+              child: Text(
+                'ACHIEVEMENT',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
             ),
-          ),
-        ],
-      ),
-      //vertical
-      Container(
+          ],
+        ),
+        Container(
           margin: EdgeInsets.only(top: 3),
           width: MediaQuery.of(context).size.width - 60,
           height: 2,
-          color: Color(0xff8CDBD8)),
-           achivementsData.isNotEmpty?
-                SizedBox(height: 200,
-                child: ListView.builder(
-                itemCount: achivementsData.length,
-                itemBuilder: (context,index){
-               return Container(
-                 
-                 child: Image.network('${achivementsData[index]}',width: 70,),);
-                },
-                            
-              ),
-           ):Center(child: CircularProgressIndicator()),
-    ],);
+          color: Color(0xff8CDBD8),
+        ),
+        achievementsData.isNotEmpty ?
+        SizedBox(
+          height: 200,
+          child: ListView.builder(
+            itemCount: achievementsData.length,
+            itemBuilder: (context, index) {
+              return Container(
+                child: Image.network(
+                  '${achievementsData[index]}',
+                  width: 70,
+                ),
+              );
+            },
+          ),
+        )
+        : Center(
+          child: CircularProgressIndicator(),
+        ),
+      ],
+    );
   }
 }
-
-
